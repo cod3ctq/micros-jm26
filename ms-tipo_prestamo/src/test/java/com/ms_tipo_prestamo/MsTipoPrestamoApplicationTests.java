29026439afc50b1e270e8addc7d78fe3e5e8cdde
Antonio22Rene <jaimes_rene@hotmail.com>
2026-06-02 12:49:46 -0600

    desarrollo de ms tipo prestamo mapper excepciones validaciones constantes

---
+++ ms-tipo_prestamo/src/test/java/com/ms_tipo_prestamo/MsTipoPrestamoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms_tipo_prestamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTipoPrestamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
