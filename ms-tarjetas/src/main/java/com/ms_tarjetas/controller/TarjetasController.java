b55187d5d7a661d3b018a75778d5ea2df9d75a4f
unknown <iymrvz@gmail.com>
2026-06-03 09:09:51 -0500

    Se agrego documentacion Swagger

--- ms-tarjetas/src/main/java/com/ms_tarjetas/controller/TarjetasController.java
+++ ms-tarjetas/src/main/java/com/ms_tarjetas/controller/TarjetasController.java
@@ -4,6 +4,8 @@ import com.ms_tarjetas.dto.TarjetasRequest;
 import com.ms_tarjetas.dto.TarjetasResponse;
 import com.ms_tarjetas.entity.Tarjetas;
 import com.ms_tarjetas.logic.TarjetaLogic;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+@Tag(name="Tarjetas", description = "Operaciones para gestion de tarjetas")
 @RestController
 @RequestMapping("api/v1/tarjetas")//Nombre del recurso, mapeo a nivel de controlador
 public class TarjetasController {
@@ -19,12 +23,20 @@ public class TarjetasController {
     @Autowired
     TarjetaLogic service;
 
+    @Operation(
+            summary = "ListaA todas las tarjetas",
+            description = "obtiene los datos de las tarjetas"
+    )
     @GetMapping
     public ResponseEntity<List<Tarjetas>> mostrar() {
         List<Tarjetas> tarjetas = service.mostrar();
         return new ResponseEntity<List<Tarjetas>>(tarjetas, HttpStatusCode.valueOf(200));
     }
 
+    @Operation(
+            summary = "Guardar Tarjeta",
+            description ="Registra una nueva tarjeta usando un DTO de entrada"
+            )
     @PostMapping
     public ResponseEntity<TarjetasResponse> guardar(@Valid @RequestBody TarjetasRequest request) {
         System.out.println("ENTRO AL CONTROLLER>>>>>> " + request);
@@ -32,18 +44,30 @@ public class TarjetasController {
         return new ResponseEntity<TarjetasResponse>(tarjeta, HttpStatusCode.valueOf(200));
     }
 
+    @Operation(
+            summary = "busca tarjetas",
+            description = "obtiene los datos de las tarjetas por el id"
+    )
     @GetMapping("/buscar/{id}")
     public ResponseEntity<TarjetasResponse> buscar(@PathVariable int id) {
         TarjetasResponse t = service.buscar(id);
         return new ResponseEntity<TarjetasResponse>(t, HttpStatusCode.valueOf(200));
     }
 
+    @Operation(
+            summary = "elimina tarjetas",
+            description = "elimina los datos de las tarjetas"
+    )
     @DeleteMapping("eliminar/{id}")
     public ResponseEntity<String> eliminar(@PathVariable int id) {
         String mensaje = service.eliminar(id);
         return new ResponseEntity<String>(mensaje, HttpStatusCode.valueOf(200));
     }
 
+    @Operation(
+            summary = "actualiza tarjetas",
+            description = "actualiza los datos de las tarjetas y los guarda"
+    )
     @PutMapping
     public ResponseEntity<TarjetasResponse> actualizar(@Valid @RequestBody TarjetasRequest request) {
         TarjetasResponse t = service.actualizar(request);
--- ms-tarjetas/src/main/java/com/ms_tarjetas/dto/TarjetasResponse.java
+++ ms-tarjetas/src/main/java/com/ms_tarjetas/dto/TarjetasResponse.java
@@ -1,12 +1,20 @@
 package com.ms_tarjetas.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDate;
 
 //mapea los objetos de salida del servicio hacia el cliente
+@Schema(description = "DTO de respuesta con los datos procesados de la tarjeta")
 public class TarjetasResponse {
 
+   @Schema(description = "Numero de tarjeta de cliente", example = "5273849462736452")
     private String numTarjeta;
+
+   @Schema(description = "Fecha de vencimiento asociada a la tarjeta")
     private LocalDate fechaVen;
+
+   @Schema(description = "Estado de la tarjeta", example = "BLOQUEADO")
     private String status;
 
     public TarjetasResponse(String numTarjeta, LocalDate fechaVen, String status) {
