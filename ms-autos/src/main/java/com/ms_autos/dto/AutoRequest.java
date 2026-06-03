91674bdb6ef8123c65b6c47988de818512f36b06
cod3ctq <onlyforcodectq1@gmail.com>
2026-06-03 09:24:37 -0600

    documentacion swagger ms autos

--- ms-autos/src/main/java/com/ms_autos/dto/AutoRequest.java
+++ ms-autos/src/main/java/com/ms_autos/dto/AutoRequest.java
@@ -1,26 +1,34 @@
 package com.ms_autos.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 
 // 3
 //DTO: Transmision datos
 //Se llama Request (peticion) por que mapea los datos que llegaran en las peticiones hacia este servicio
+@Schema(description = "DTO para registrar o actualizar un auto")
 public class AutoRequest {
+
+    @Schema(description = "Identificador del auto", example = "1")
     private int autoId;
 
+    @Schema(description = "Descripcion del modelo del auto", example = "Corvette C6")
     @NotBlank(message="El modelo no puede quedar vacio o nulo")
     @Size(message="El modelo permite hasta 15 caracteres", max = 15)
     private String modelo;
 
+    @Schema(description = "Año de fabricacion del auto", example = "2013")
     @Digits(integer = 4, fraction = 0)
     @Min(value=1980, message = "El año del auto no debe ser menor a 1980")
     private int año;
 
+    @Schema(description = "Categoria o segmento del auto", example = "Deportivo")
     @NotBlank(message="El tipo no puede quedar vacio o nulo")
     @Size(message="El tipo permite hasta 15 caracteres", max = 15)
     private String tipo;
 
+    @Schema(description = "Identificador de la marca del auto", example = "1")
     @Digits(integer = 1, fraction = 0)
     private int marcaId;
 
--- ms-autos/src/main/java/com/ms_autos/dto/AutoResponse.java
+++ ms-autos/src/main/java/com/ms_autos/dto/AutoResponse.java
@@ -1,13 +1,23 @@
 package com.ms_autos.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDate;
 
-//Se llama Response (Respuesta) por que apean los objetos de salida del servicio hacia el cliente
+//Se llama Response (Respuesta) por que mapean los objetos de salida del servicio hacia el cliente
+@Schema(description = "DTO de respuesta con los datos procesados del auto")
 public class AutoResponse {
 
+    @Schema(description = "Descripcion del modelo del auto", example = "Camaro LS")
     private String modelo;
+
+    @Schema(description = "Año del auto", example = "2020")
     private int año;
+
+    @Schema(description = "Tipo del auto", example = "Muscle Car")
     private String tipo;
+
+    @Schema(description = "Fecha del guardado del auto", example = "01/01/2022")
     private LocalDate fechaRegistro;
 
     public AutoResponse(String modelo, int año, String tipo, LocalDate fechaRegistro) {
