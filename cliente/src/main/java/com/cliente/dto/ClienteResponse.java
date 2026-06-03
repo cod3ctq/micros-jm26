46902c525232039adeaad624bebbe03fe70113a8
LuckyThrym <lgsainzr@gmail.com>
2026-06-03 09:02:51 -0600

    Documentacion Swagger Cliente

--- cliente/src/main/java/com/cliente/dto/ClienteResponse.java
+++ cliente/src/main/java/com/cliente/dto/ClienteResponse.java
@@ -1,14 +1,21 @@
 package com.cliente.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDate;
 
 //Mapean los objetos de salida del servicio hacia el cliente
+@Schema(description = "DTO de respuesta con los datos procesados del cliente")
 public class ClienteResponse {
 
+    @Schema(description = "Nombre del cliente", example = "Carlos")
     private String nombre;
+    @Schema(description = "Apellido paterno del cliente", example = "Sanchez")
     private String apP;
+    @Schema(description = "Apellido materno del cliente", example = "Ramirez")
     private String apM;
+    @Schema(description = "Fecha de nacimiento del cliente", example = "28-03-2000")
     private LocalDate fechaNac;
 
     public ClienteResponse(String nombre, String apP, String apM, LocalDate fechaNac) {
