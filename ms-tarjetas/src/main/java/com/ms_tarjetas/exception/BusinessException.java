d9e73dd455e7a065f15b75cf05d8eb5eeb54b133
unknown <iymrvz@gmail.com>
2026-06-04 12:22:42 -0500

    Contruccipon de malla de servicios con api gateway y service registry yrz

--- ms-tarjetas/src/main/java/com/ms_tarjetas/exception/BusinessException.java
+++
@@ -1,8 +0,0 @@
-package com.ms_tarjetas.exception;
-
-//Esta clase representa cualquier excepcion que desee propagar en una validacion de negocio
-public class BusinessException extends RuntimeException {
-    public BusinessException(String message) {
-        super(message);
-    }
-}
