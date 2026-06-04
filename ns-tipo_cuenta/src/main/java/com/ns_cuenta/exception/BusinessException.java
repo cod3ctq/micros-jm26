6f56ef8facdd8eb2faa189498e38cc328379fea1
josue-borja17 <josueborja170799@gmail.com>
2026-06-04 12:22:46 -0600

    construccion de malla de servicios con api gateway y service registry jba

--- ns-tipo_cuenta/src/main/java/com/ns_cuenta/exception/BusinessException.java
+++
@@ -1,9 +0,0 @@
-package com.ns_cuenta.exception;
-
-
-//Esta clase representa cualquier excepcion que desee propagar en una validacion de negocio. 
-public class BusinessException extends RuntimeException {
-    public BusinessException(String message) {
-        super(message);
-    }
-}
