20a49906959b9a0d2e65bc0269ee9e6b18df69ab
CarlosOC94 <carlosoc94@hotmail.com>
2026-06-04 12:22:21 -0600

    Construcción de malla de microservicios con ape gateway y service registry coc

--- ms-empleados/src/main/java/com/ms_empleados/constants/Mensajes.java
+++
@@ -1,6 +0,0 @@
-package com.ms_empleados.constants;
-
-public class Mensajes {
-    public static final String LIMITE_ROL = "Límite de empleados con ese rol alcanzado";
-    public static final String LIMITE_APELLIDO_P = "Límite de empleados con el mismo apellido alcanzado";
-}
--- ns-tipo_cuenta/src/main/java/com/ns_cuenta/constants/Mensajes.java
+++
@@ -1,9 +0,0 @@
-package com.ns_cuenta.constants;
-
-public class Mensajes {
-
-
-    public static final String LIMITE_SALDOMIN ="LIMITE DE SALDO MIN ALACANZADO";
-    public static final String LIMITE_SALDOMAX ="LIMITE DE SALDO MAX ALACANZADO";
-
-}
