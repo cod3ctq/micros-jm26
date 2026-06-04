70ceb1816213da06d17da9544fabcfdecb4182d3
josuea11y <josuevazqza3006@gmail.com>
2026-06-04 12:22:32 -0600

    construccion de malla servicios con api gateaway y service registry jvz

--- ms-medios_pago/src/main/java/com/ms_medios_pago/constants/Mensajes.java
+++
@@ -1,5 +0,0 @@
-package com.ms_medios_pago.constants;
-
-public class Mensajes {
-    public static final String DESCRIPCION_DUPLICADA = "No puede haber dos medios de pago con la misma descripcion";
-}
